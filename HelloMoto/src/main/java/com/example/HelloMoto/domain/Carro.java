bfddfc31d2b5e6c99e7803d34c1c26a8ba807e75
TomSpinelli <55303510+TomSpinelli@users.noreply.github.com>
2019-09-21 11:24:26 -0300

    V.1.0 ConfigInfra CRUD
    V.1.0 ConfigInfra CRUD

---
+++ HelloMoto/src/main/java/com/example/HelloMoto/domain/Carro.java
@@ -0,0 +1,62 @@
+package com.example.HelloMoto.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Carro {
+
+	@Column(name = "nome")
+	private String nome;
+	
+	private String tipo;
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	@Id 
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	public Carro() {
+		
+	}
+	
+	public Carro(String nome, Long id) {
+		super();
+		this.nome = nome;
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
